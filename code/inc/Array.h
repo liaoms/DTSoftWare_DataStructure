83c35ada81cdbb35100439e780af94cf19cfc8cb
liaoms <763905926@qq.com>
2020-06-10 11:35:03 +0800

    数组类基类Array.h实现

---
+++ code/inc/Array.h
@@ -0,0 +1,27 @@
+#ifndef __ARRAY_H__
+#define __ARRAY_H__
+
+#include "Object.h"
+
+namespace LMSLib
+{
+
+template <typename T>
+class Array : public Object
+{
+public:
+    virtual bool set(int index, const T& e) = 0;
+    virtual bool get(int index, T& e) = 0;
+    virtual T& operator [](int index) = 0;
+    virtual T operator [](int index) const = 0;
+    virtual int length() = 0;
+
+protected:
+    T* m_array;
+    int m_length;
+};
+
+}
+
+
+#endif // !__ARRAY_H__
